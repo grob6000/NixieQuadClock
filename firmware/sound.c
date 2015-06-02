31bed55d217a56e521ce04f25492ad53242622e2
grob6000 <grobinson6000@gmail.com>
2015-06-02 13:03:11 +1000

    Create sound module files

---
+++ firmware/sound.c
@@ -0,0 +1,28 @@
+/*
+ * sound.c
+ *
+ * Created: 2015-06-02 09:55:32
+ *  Author: george
+ */ 
+
+#include "sound.h"
+
+void AlarmOn()
+{
+	
+}
+
+void AlarmOff()
+{
+	
+}
+
+void MakeBeep()
+{
+	
+}
+
+void MakeClick()
+{
+	
+}
---
+++ firmware/sound.h
@@ -0,0 +1,17 @@
+/*
+ * sound.h
+ *
+ * Created: 2015-06-02 09:44:56
+ *  Author: george
+ */ 
+
+
+#ifndef SOUND_H_
+#define SOUND_H_
+
+void AlarmOn();
+void AlarmOff();
+void MakeBeep();
+void MakeClick();
+
+#endif /* SOUND_H_ */
